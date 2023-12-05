142e59308745980e90bbef33feb25fbd86cdfb81
Даниил Палабугин <dan.palabugin@yandex.ru>
2023-12-05 12:22:31 +0400

    fix search skeleton

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -21,7 +21,7 @@ const MenuProps = {
 };
 
 const skeleton = (
-  <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+  <div className="grid gap-4 grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
     {[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map((el, i) => (
       <MovieSceleton key={i} />
     ))}
